9fa885dd18b8fb4565a1f349ec676768ba428045
tangxuyang <tangxuyang@lifang.com>
2016-07-19 19:36:33 +0800

    修改引用路径

--- public/components/web/body.php
+++ public/components/web/body.php
@@ -2,7 +2,7 @@
 <!--web-container包含三部分——header,content和footer-->
 <div class="web-container">
 <?php
-	require_once("header.php");
+	require_once("../public/components/web/header.php");
 	require_once("../public/components/web/content.php");
 	require_once("../public/components/web/footer.php");
 ?>
