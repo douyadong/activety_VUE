89a6d4ae5d55cbd5e38f647ebb7e9e1a610bec73
Kingsley <Yu@KingsleydeMacBook-Pro.local>
2016-08-04 23:22:23 +0800

    fix android 键盘遮挡的问题

--- olympic/jssrc/index.js
+++ olympic/jssrc/index.js
@@ -179,6 +179,17 @@ IndexController.prototype.bindEvent = function() {
 
         classSelf.displayDialog();
     });
+
+    if (/Android/gi.test(navigator.userAgent)) {
+        window.addEventListener('resize', function() {
+            if (document.activeElement.tagName == 'INPUT' || document.activeElement.tagName == 'TEXTAREA') {
+                window.setTimeout(function() {
+                    document.activeElement.scrollIntoViewIfNeeded();
+                }, 0);
+            }
+        })
+    }
+
 };
 
 
