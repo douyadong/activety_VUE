f0d3aae94e5c64cdee7e0f5a6c5b1779e181af82
Kingsley <Yu@KingsleydeMacBook-Pro.local>
2016-09-07 20:39:23 +0800

    update

--- public/global.php
+++ public/global.php
@@ -30,14 +30,16 @@
     }    
     //静态资源域名
     $STATIC_DOMAIN = get_stage_domain(array(
-        "dev"=>"//dev01.fe.wkzf",
+        // "dev"=>"//dev01.fe.wkzf",
+        "dev"=>"//10.0.92.98/activity",       
         "test"=>"//test01.fe.wkzf",
         "sim"=>"//sim01.fe.wkzf",
         "prod"=>"//cdn01.wkzf.com"
     )); 
     //当前域名      
     $CURRENT_STATIC_DOMAIN =  get_stage_domain(array(
-        "dev"=>"//devhd.fe.wkzf",
+        // "dev"=>"//devhd.fe.wkzf",
+        "dev"=>"http://10.0.92.98/activity",
         "test"=>"//testhd.fe.wkzf",
         "sim"=>"//simhd.fe.wkzf",
         "prod"=>"//hd.wkzf.com"
