c7c705da92a639dde4e4dd10f451441d5dfc4f55
Anna <yinqin@lifang.com>
2016-12-19 18:01:52 +0800

    圣诞活动

---
+++ christmas/index.php
@@ -0,0 +1,175 @@
+<?php
+        ob_start();
+    ?>
+<!DOCTYPE html>
+<html lang="zh-cn">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Merry Christmas 我愿为你种星辰</title>
+    <meta name="keywords" content="悟空找房,Merry Christams">
+    <meta name="description" content="Merry Christmas 我愿为你种星辰">
+    <link rel="stylesheet" href="../public/css/app.min.css">
+    <link rel="stylesheet" href="./css/index.min.css">
+    <link rel="stylesheet" href="http://cdn01.wkzf.com/fe_public_library/wkzf/js/util/swiper/dist/css/swiper.min.css">
+</head>
+
+<body>
+    <section id="loading" style="display:none">
+        <div id="man">
+            <img src="images/bobsleigh.png">
+        </div>
+        <div id="process">
+            <div id="processInner"></div>
+        </div>
+        <div id="loadfont">0%</div>
+        <div id="start">
+            <img src="images/start.png">
+        </div>
+    </section>
+    <section id="content" class="bg_1">
+        <div class="text">
+        </div>
+        <div id="menu">
+            <a data-number="1">
+                <img src="images/choose_bg.png">
+                <span>选张卡片</span>
+            </a>
+            <a data-number="2">
+                <img src="images/choose_text.png">
+                <span>选个祝福</span>
+            </a>
+            <a id="submit">
+                送出祝福<img src="images/sanjiao.jpg">
+            </a>
+        </div>
+    </section>
+    <div>
+        <a id="makeup" href="/christmas/index.html">
+            <div>
+                我也要制作悟空圣诞星辰卡<img src="images/sanjiao.jpg">
+            </div>
+        </a>
+    </div>
+    <div id="chooseBg">
+        <div class="choose_content">
+            <div class="bg-slider">
+                <div id="swiperBg" class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide" data-number="1">
+                            <img src="images/bg_1.gif">
+                        </div>
+                        <div class="swiper-slide" data-number="2">
+                            <img src="images/bg_2.gif">
+                        </div>
+                        <div class="swiper-slide" data-number="3">
+                            <img src="images/bg_3.gif">
+                        </div>
+                        <div class="swiper-slide" data-number="4">
+                            <img src="images/bg_4.gif">
+                        </div>
+                    </div>
+                    <!-- 如果需要导航按钮 -->
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+                </div>
+                <div class="lick-btn" data-number="1">
+                    喜欢这个<img src="images/arrow_2.png">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="chooseText">
+        <div class="choose_content">
+            <div class="bg-slider">
+                <div id="swiperText" class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide" data-number="1">
+                            <img src="images/text_1.png">
+                        </div>
+                        <div class="swiper-slide" data-number="2">
+                            <img src="images/text_4.png">
+                        </div>
+                        <div class="swiper-slide" data-number="3">
+                            <img src="images/text_3.png">
+                        </div>
+                        <div class="swiper-slide" data-number="4">
+                            <img src="images/text_4.png">
+                        </div>
+                    </div>
+                    <!-- 如果需要导航按钮 -->
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+                </div>
+                <div class="lick-btn" data-number="2">
+                    喜欢这个<img src="images/arrow_2.png">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--同意条款-->
+    <div id="contract">
+        <div class="contract_content">
+            <p>
+                <input type="checkbox" checked name="contract">本人已阅读并同意
+            </p>
+            <a href="javascript:;" id="showItems">活动规则</a>
+            <a href="javascript:;" id="submitContrct">确定</a>
+        </div>
+    </div>
+    <!--提示-->
+    <div id="tips">
+        <div class="contract_content">
+            <p>
+            </p>
+            <a href="javascript:;" id="tipsSubmit">确定</a>
+        </div>
+    </div>
+    <!--条款内容-->
+    <div id="items">
+        <p class="title">活动时间</p>
+        <p>2016.12.22-2016.12.26</p>
+        <p class="title">活动条款</p>
+        <p>感谢您参与悟空找房2016圣诞贺卡活动（下称“本活动”）。请仔细阅读下面的活动条款。</p>
+        <p class="title">一、活动条款的确认和接受</p>
+        <p>本活动由悟空找房举办，与本活动有关的所有权利均由主办方所有。本活动将按照本条款执行。本活动的参与者（简称为“用户”）通过本活动制作及/或分享圣诞贺卡，即表示用户已经接受本活动所有条款。</p>
+        <p class="title">二、用户的承诺及义务</p>
+        <p>1. 用户确保其参与活动并制作、分享的圣诞贺卡符合公序良俗，遵守法律法规的规定，不存在任何侵犯主办方或第三方合法权益的内容和信息。</p>
+        <p>2. 用户因从事任何上述行为而给主办方或他人造成的损害，用户应自行承担相关责任并应赔偿主办方全部损失，一经发现用户违反本活动条款，主办方有权在任何时间、不经事先通知，而对相关作品进行删除并取消用户参与活动的资格。</p>
+        <p>3. 用户认可，在按本活动条款提交信息时，即允许主办方及其他关联公司作为宣传及促销等目的的需要以任何方式使用用户提交的信息的全部或部分及对该信息的编辑或汇编等，包括但不限于用户的姓名或其他名称。</p>
+        <p class="title">三、免责声明</p>
+        <p>1. 主办方保留根据客观情况不时地修改本活动条款的权利。</p>
+        <p>2. 主办方将尽力确保本活动的顺利进行，但若因特定原因（包括但不限于设备或系统维护、互联网络、电脑、通讯或其他系统的故障，行政决定或第三方的不作为等）而使本活动因此无法进行时，主办方有权暂停或取消本活动，且主办方不承担任何法律责任。</p>
+        <p>3. 未经主办方授权或许可的，任何单位或个人不得通过任何形式，以任何方法使用、截取、篡改本活动中产生的活动页面或页面（包括HTML5页面），或采取任何其他未经授权或许可的行为。主办方保留与此相关的一切权利。</p>
+        <p class="title">四、活动相关内容的所有权</p>
+        <p>与本次活动相关的内容，包括但不限于文字、软件、声音、音乐、相片、录像、图标，在广告中出现的全部内容，电子邮件的全部内容，本活动过程中为用户提供的商业信息等受版权、商标和其他财产所有权法律的保护。用户不得擅自复制、篡改、使用相关商标、内容或创造有关的派生产品。</p>
+        <p class="title">在法律允许的范围内，悟空找房有权对上述活动的条款作出合理解释和/或补充说明。</p>
+        <a href="javascript:;" id="itemSubmit">我知道了</a>
+    </div>
+    <!--背景-->
+    <input type="hidden" value="bg_1" name="bg">
+    <!--祝福-->
+    <input type="hidden" value="" name="text">
+    <!--姓名-->
+    <input type="hidden" value="" name="username">
+    <!--微信分享标题-->
+    <input type="hidden" value="送出一份暖心圣诞祝福" name="wechatTitle">
+    <!--微信分享内容-->
+    <input type="hidden" value="把你的星星种进悟空「圣诞星辰卡」，送给TA吧。" name="wechatContent">
+    <!--微信分享内容-->
+    <input type="hidden" value="" name="wechatUrl">
+</body>
+<script src="../config.js"></script>
+<script src="../public/js/app.min.js"></script>
+<script src="http://cdn01.wkzf.com/fe_public_library/wkzf/js/util/swiper/dist/js/swiper.min.js"></script>
+<script src="./js/html2canvas.min.js"></script>
+<script src="./js/index.min.js"></script>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<script src="./js/wechat-share.min.js"></script>
+
+</html>
+<?php
+require_once("../public/components/save_file.php");
+?>
---
+++ christmas/jssrc/index.js
@@ -0,0 +1,252 @@
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 1. 项目名称：Merry christmas
+ 2. 页面名称：index.html
+ 3. 作者：yinqin@lifang.com
+ -----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+function IndexController() {
+    /*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     init
+     -----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+    this.init();
+    /*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     bindEvent
+     -----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+    this.bindEvent();
+    /*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     initLoading
+     -----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+    this.initLoading();
+};
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ init
+ -----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+IndexController.prototype.init = function() {
+    var classSelf = this;
+    $("[name='wechatUrl']").val(window.location.href);
+    var bg = classSelf.getQueryStringByName("bg"),
+        text = classSelf.getQueryStringByName("text"),
+        name = classSelf.getQueryStringByName("name");
+    if (bg && text && name) {
+        $("#loading").hide();
+        $("#content").show();
+        $("#content").removeAttr("class").addClass(bg);
+        $("#menu").hide();
+        $("#content").attr("text", text);
+        var htmlStr = '<img src="images/' + text + '.png">\
+                            <div>\
+                                ' + name + '\
+                            </div>';
+        $("#content").find(".text").html('').html(htmlStr).find("div").show();
+        $("#makeup").show();
+        $("[name='text']").val(classSelf.getQueryStringByName('text'));
+        $("[name='username']").val(classSelf.getQueryStringByName("name"));
+        $("[name='wechatTitle']").val('Merry Christmas 我愿为你种星辰');
+        $("[name='wechatUrl']").val(window.location.href);
+        classSelf.html2Canvans();
+        $("#content").hide();
+
+    } else {
+        $("#loading").show();
+        $("#content").hide();
+    }
+};
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ html2Canvans
+ -----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+IndexController.prototype.html2Canvans = function() {
+    html2canvas(document.body, {
+        onrendered: function(canvas) {
+            document.body.appendChild(canvas);
+        }
+    });
+};
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ initChooseBg
+ -----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+IndexController.prototype.initChooseBg = function() {
+    var mySwiper = new Swiper('#swiperBg', {
+        direction: 'horizontal',
+        loop: true,
+        // 如果需要前进后退按钮
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        onSlideChangeEnd: function(swiper, event) {
+            var index = swiper.activeIndex;
+            if (swiper.activeIndex == 5) {
+                index = 1;
+            }
+            $(".text").html('');
+            $("#content").removeAttr("class").addClass("bg_" + index);
+        }
+    });
+};
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ initChooseText
+ -----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+IndexController.prototype.initChooseText = function() {
+    var mySwiper2 = new Swiper('#swiperText', {
+        direction: 'horizontal',
+        loop: true,
+        // 如果需要前进后退按钮
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        onSlideChangeEnd: function(swiper, event) {
+            $("[name='username']").val('');
+            var index = swiper.activeIndex;
+            if (swiper.activeIndex == 5) {
+                index = 1;
+            }
+            var htmlStr = '<img src="images/text_' + index + '.png">\
+                            <div>\
+                                <i></i>\
+                                <input type="text" placeholder="输入姓名" id="name">\
+                                <p>(输入10个字符以内的名字)</p>\
+                            </div>';
+            $("#content").removeAttr("text").attr("text", "text_" + index);
+            $("#content").find(".text").html('').html(htmlStr);
+        }
+    })
+};
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ bindEvent
+ -----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+IndexController.prototype.bindEvent = function() {
+    var classSelf = this;
+    //选卡片点击事件
+    $("#menu a[data-number=1]").click(function() {
+        $("#chooseBg").show();
+        $("[name='bg']").val("bg_1");
+        $("[name='text']").val("");
+        classSelf.initChooseBg();
+    });
+    //选祝福点击事件
+    $("#menu a[data-number=2]").click(function() {
+        $("#chooseText").show();
+        classSelf.initChooseText();
+    });
+
+    //喜欢背景/祝福点击事件
+    $(".lick-btn").click(function() {
+        var dataNumber = $(this).attr("data-number");
+        //1是背景  2是祝福
+        if (dataNumber == "1") {
+            $("[name = 'bg']").val($("#content").attr("class"));
+            $("#chooseBg").hide();
+        } else {
+            $("[name = 'text']").val($("#content").attr("text"));
+            $("#chooseText").hide();
+            $(".text > div").fadeIn('slow');
+        }
+    });
+    //点击开始制作
+    $("#start").click(function() {
+        $("#loading").hide();
+        $("#content").fadeIn();
+    });
+
+    //阅读活动规则
+    $("#showItems").click(function() {
+        $("#items").fadeIn();
+    })
+
+    $("#items").click(function() {
+        $("#items").fadeOut();
+    });
+    //已阅读规则确定
+    $("#submitContrct").click(function() {
+        if (!$("[name='contract']").is(':checked')) {
+            classSelf.tips("请勾选并同意！");
+            $("#contract").hide();
+        } else {
+            $("#contract").hide();
+            $("#content .text>div").html('').html($("[name='username']").val());
+            $("[name='wechatTitle']").val("Merry Christmas 我愿为你种星辰");
+            $("[name='wechatContent']").val($("[name='username']").val() + "已经把对你的祝福种进悟空「圣诞星辰卡」，快打开看看吧~");
+            $("[name='wechatUrl']").val(window.location.href + "?bg=" + $("[name='bg']").val() + "&text=" + $("[name='text']").val() + "&name=" + $("[name='username']").val());
+            $("#menu").hide();
+            classSelf.html2Canvans();
+            $("#content").hide();
+            $("#makeup").show();
+        }
+    });
+    //输入姓名事件
+    $("body").delegate('#name', 'change', function() {
+        $("[name='username']").val($(this).val());
+    });
+
+    $("#tipsSubmit").click(function() {
+            $("#tips").fadeOut();
+            if (!$("[name='contract']").is(':checked')) {
+                $("#contract").show();
+            }
+        })
+        //送出祝福
+    $("#submit").click(function() {
+        //检查是否选了祝福语
+        if (!$("[name='text']").val()) {
+            classSelf.tips("请先选个祝福!");
+            return false;
+        }
+
+        //检查姓名
+        if (!$("[name='username']").val()) {
+            classSelf.tips("请填写姓名!");
+            return false;
+        }
+        //检查是否10个字
+        var reg = /^[0-9a-zA-Z\u4e00-\u9fa5]{1,10}$/;
+        if (!reg.test($("#name").val())) {
+            classSelf.tips("姓名最多10个英文字符!");
+            return false;
+        }
+
+        $("#contract").fadeIn();
+    })
+};
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ initLoading
+ -----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+IndexController.prototype.initLoading = function() {
+    //3s #processInner 执行完毕
+    var timer = setInterval(function() {
+        var currentWidth = $("#processInner").width();
+        var loadingText = parseInt($("#loadfont").text().split("%")[0]);
+        if (currentWidth >= 200) {
+            clearInterval(timer);
+            $("#man").fadeOut();
+            $("#process").fadeOut();
+            $("#loadfont").fadeOut();
+            var timeout = setTimeout(function() {
+                $("#start").css("bottom", "10px");
+            }, 300);
+        } else {
+            $("#processInner").width(currentWidth + 2);
+            $("#loadfont").text((loadingText + 1) + "%");
+            $("#man img").css("margin-left", currentWidth + 2);
+        }
+    }, 8);
+};
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 根据QueryString参数名称获取值
+ -----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+IndexController.prototype.getQueryStringByName = function(name) {
+    var result = location.search.match(new RegExp("[\?\&]" + name + "=([^\&]+)", "i"));
+    if (result == null || result.length < 1) {
+        return "";
+    }
+    return result[1];
+};
+
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ tips
+ -----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+IndexController.prototype.tips = function(text) {
+    $("#tips").show();
+    $("#tips p").text(text);
+};
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+类的初始化
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+$(document).ready(function() {
+    new IndexController;
+});
