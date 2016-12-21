39fa813b8789612a7ab74f7fa73fe9df4d9846d5
Anna <yinqin@lifang.com>
2016-12-21 16:36:28 +0800

    微信截图

--- christmas/jssrc/wechat-share.js
+++ christmas/jssrc/wechat-share.js
@@ -73,7 +73,7 @@ function WechatShareController() {
             dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
             success: function() {
                 // 用户确认分享后执行的回调函数
-                alert("wx_onMenuShareAppMessage success " + linkUrl);
+                //alert("wx_onMenuShareAppMessage success " + linkUrl);
             },
             cancel: function() {
                 // 用户取消分享后执行的回调函数
