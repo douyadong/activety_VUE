5bb7c4daef4a775b74aabe3f778aad7f99a762e5
Anna <yinqin@lifang.com>
2016-12-22 15:24:04 +0800

    图片

--- christmas/jssrc/index.js
+++ christmas/jssrc/index.js
@@ -20,6 +20,24 @@ function IndexController() {
     this.mySwiper2 = null;
     this.mySwiper = null;
 };
+IndexController.prototype.dataURItoBlob = function(dataURI, callback) {
+    // convert base64 to raw binary data held in a string
+    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+    var byteString = atob(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to an ArrayBuffer
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    // write the ArrayBuffer to a blob, and you're done
+    var bb = new Blob([ab]);
+    return bb;
+};
 
 IndexController.prototype.createMask = function() {
     //获取页面高度和宽度
@@ -85,8 +103,16 @@ IndexController.prototype.html2Canvans = function() {
         onrendered: function(canvas) {
             document.body.appendChild(canvas);
             var img = classSelf.convertCanvasToImage(document.getElementsByTagName("canvas")[0]);
+            $(img).addClass("save-img");
             $(document.getElementsByTagName("canvas")[0]).hide();
-            $("body").append(img).addClass("save-img");
+            $("body").append(img);
+
+            var blob = classSelf.dataURItoBlob($(".save-img").attr("src"));
+
+            var url = window.URL.createObjectURL(blob);
+
+            $(".save-img").attr("src", url);
+
         },
         width: document.documentElement.scrollWidth * 2
     });
